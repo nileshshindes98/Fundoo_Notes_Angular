7dc58832f5fad913e01904617053919a023f13ab
nileshshindes98 <nileshshindes98@gmail.com>
2023-10-19 18:17:43 +0530

    [Nilesh] : add required Module for Components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,25 +14,55 @@ import {MatButtonModule} from '@angular/material/button';
 import {FormsModule, ReactiveFormsModule} from '@angular/forms';
 // import {NgIf} from '@angular/common';
 import {MatCheckboxModule} from '@angular/material/checkbox';
+import {HttpClientModule} from '@angular/common/http';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+import {MatSidenavModule} from '@angular/material/sidenav';
+import {MatSelectModule} from '@angular/material/select';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import { ResetPasswordComponent } from './components/reset-password/reset-password.component';
+import {MediaMatcher} from '@angular/cdk/layout';
+import {ChangeDetectorRef, Component, OnDestroy} from '@angular/core';
+import {MatListModule} from '@angular/material/list';
+
+import {NgIf, NgFor} from '@angular/common';
+
+import { CreateNoteComponent } from './components/create-note/create-note.component';
+import { GetAllNotesComponent } from './components/get-all-notes/get-all-notes.component';
+import { DisplayNotesComponent } from './components/display-notes/display-notes.component';
+import {MatMenuModule} from '@angular/material/menu';
+
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
-    RegisterComponent
+    RegisterComponent,
+    DashboardComponent,
+    ResetPasswordComponent,
+    CreateNoteComponent,
+    GetAllNotesComponent,
+    DisplayNotesComponent,
   ],
   imports: [
-    BrowserModule,
     AppRoutingModule,
+    BrowserModule,
     FormsModule, ReactiveFormsModule,
+    HttpClientModule,
     BrowserAnimationsModule,
     MatFormFieldModule,
     MatInputModule,
     MatIconModule,
     MatCardModule,
     MatCheckboxModule,
+    MatSidenavModule,
     MatGridListModule,
-    MatButtonModule,
+    MatMenuModule,
+     MatFormFieldModule, MatSelectModule, MatButtonModule,
+   
+    MatToolbarModule,
+    
+   
+    MatListModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
