29444977c2c129c49918e1f269f71e5c5b3f04d8
nileshshindes98 <nileshshindes98@gmail.com>
2023-10-19 18:22:17 +0530

    [Nilesh] : created get all note comp. which is parent comp. of create note and display note comp.

---
+++ src/app/components/get-all-notes/get-all-notes.component.ts
@@ -0,0 +1,24 @@
+import { Component } from '@angular/core';
+import { NoteService } from 'src/app/service/noteService/note.service';
+@Component({
+  selector: 'app-get-all-notes',
+  templateUrl: './get-all-notes.component.html',
+  styleUrls: ['./get-all-notes.component.scss']
+})
+export class GetAllNotesComponent {
+//get all notes API in ngOnit
+//1) display notes 
+//2) get all note api & 
+//3) get all cha HTML mdhe array print karaycha using ngFor
+//
+
+constructor(private note:NoteService ){}
+token:any;
+notesArray: any = [];
+addNoteData(){
+// this.data.push()
+this.note.GetallNotes(this.token).subscribe((notesData:any)=>{
+  // this.notesArray = notesData.data.data.
+})
+}
+}
