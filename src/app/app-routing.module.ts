e1059022dc1c8849b101678230bd353f953bd9a1
nileshshindes98 <nileshshindes98@gmail.com>
2023-10-19 18:22:41 +0530

    [Nilesh] : add rounting for pages

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,13 +2,30 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './components/login/login.component';
 import { RegisterComponent } from './components/register/register.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { ResetPasswordComponent } from './components/reset-password/reset-password.component';
+import { GetAllNotesComponent } from './components/get-all-notes/get-all-notes.component';
 
 const routes: Routes = [
+  // {
+  //   path : '',redirectTo:'/login',pathMatch:'full'
+  // },
   {
-    path : '',component:LoginComponent
+    path : '', component:LoginComponent,
   },
   {
-    path : 'signup',component:RegisterComponent
+    path : 'signup',component:RegisterComponent,
+  },
+  {
+    path: 'dashboard',component:DashboardComponent,
+    children:[
+      {
+        path:"notes",component:GetAllNotesComponent,
+      }
+    ]
+  },
+  {
+    path :'resetPassword',component:ResetPasswordComponent,
   }
 ];
 
