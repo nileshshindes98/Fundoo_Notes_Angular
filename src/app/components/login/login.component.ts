9a39386b1cca26fe0356ee7ef4ebf9b4c4c6bcc9
nileshshindes98 <nileshshindes98@gmail.com>
2023-10-19 17:57:07 +0530

    [Nilesh] : Login page with data binding and API integration

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,22 +1,48 @@
 import { Component } from '@angular/core';
-import {FormControl, Validators } from '@angular/forms';
+// import {FormControl, Validators } from '@angular/forms';
+import { AbstractControl, ValidationErrors, FormControl, FormBuilder, FormGroup, Validators, } from '@angular/forms';
+import { UserService } from '../../service/userService/user-service.service';
+// import { MatSnackBar } from '@angular/material/snack-bar';
 
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.scss'],
-
 })
 export class LoginComponent {
   hide = true;
   email = new FormControl('', [Validators.required, Validators.email]);
 
-  getErrorMessage() {
-    if (this.email.hasError('required')) {
-      return 'You must enter a value';
+  loginForm!: FormGroup;
+
+  constructor(private formBuilder: FormBuilder, public userService: UserService) { }
+  ngOnInit() {
+    this.loginForm = this.formBuilder.group({
+      email: [''],
+      password: [''],
+      service: "advance",
+    });
+  }
+
+  onSubmit() {
+
+    let payLoad = {
+      email: this.loginForm.value.email,
+      password: this.loginForm.value.password,
+      service: this.loginForm.value.service
     }
 
-    return this.email.hasError('email') ? 'Not a valid email' : '';
+    this.userService.loginService(payLoad).subscribe((response: any) => {
+      localStorage.setItem('token', response.id)
+    })
+
+    if (this.loginForm) {
+      const formData = this.loginForm.value;
+      console.log(formData);
+    } else {
+     
+    }
+
   }
 }
